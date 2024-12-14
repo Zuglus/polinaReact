f2711473bd20de0ace1830c6174628e38faca875
Zuglus <kynterets@gmail.com>
2024-12-14 10:46:36 +0400

    react init

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './index.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <body className="bg-primary text-white">
+
+      <container className="min-h-screen">
+
+        <Header />
+
+      </container>
+
+    </body>
+  );
+}
+
+export default App;
